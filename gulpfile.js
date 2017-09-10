0a3f9f9c703d1e3b882597921c3328d45bc3735b
silverFox78 <sbarrera@gmail.com>
2017-09-10 19:54:18 -0300

    actualizacion

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    pug = require('gulp-pug2');
+gulp.task('pug', function() {
+    return gulp.src('pug/*.pug') // los archivos se encuentran en una carpeta llamada "pug"
+        .pipe(pug({ yourTemplate: 'Locals' }))
+        .pipe(gulp.dest('./')) // los archivos HTNL creados se ubicarán en la raíz del proyecto
+});
+
+gulp.task('watch', function() {
+    gulp.watch('pug/includes/*.pug', ['pug']);
+    gulp.watch('pug/*.pug', ['pug']);
+});
+gulp.task('default', ['pug', 'watch']);
